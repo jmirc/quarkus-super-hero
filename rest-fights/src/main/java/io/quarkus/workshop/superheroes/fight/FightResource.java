e66e16b9ca3347c2ccb1582fee9bc78247301019
Jerome Mirc <jmirc@expedia.com>
2022-03-02 15:09:38 -0500

    Add timeout

--- rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
+++ rest-fights/src/main/java/io/quarkus/workshop/superheroes/fight/FightResource.java
@@ -1,5 +1,7 @@
 package io.quarkus.workshop.superheroes.fight;
 
+import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.eclipse.microprofile.faulttolerance.Timeout;
 import org.jboss.logging.Logger;
 
 import javax.inject.Inject;
@@ -22,9 +24,22 @@ public class FightResource {
     @Inject
     FightService service;
 
+    @ConfigProperty(name = "process.milliseconds", defaultValue = "0")
+    long tooManyMilliseconds;
+
+    private void veryLongProcess() {
+        try {
+            Thread.sleep(tooManyMilliseconds);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+    }
+
     @GET
     @Path("/randomfighters")
+    @Timeout(500)
     public Response getRandomFighters() {
+        veryLongProcess();
         Fighters fighters = service.findRandomFighters();
         logger.debug("Get random fighters " + fighters);
         return Response.ok(fighters).build();
